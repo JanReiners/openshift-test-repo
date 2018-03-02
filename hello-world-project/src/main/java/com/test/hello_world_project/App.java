0a71f5223e432680944bc60b8bd64ee9cfa5ce8c
JanReiners <jan.reiners@provinzial.com>
2018-03-02 15:19:22 +0100

    Update App.java
    fix sysout

--- hello-world-project/src/main/java/com/test/hello_world_project/App.java
+++ hello-world-project/src/main/java/com/test/hello_world_project/App.java
@@ -2,7 +2,7 @@ package com.test.hello_world_project;
 
 public class App {
    public static void main(String[] args) {
-      System.out.println("Hello World!");
+      System.out.println("Hello World 1!");
 
       System.out.println("Hello World 2!");
 
