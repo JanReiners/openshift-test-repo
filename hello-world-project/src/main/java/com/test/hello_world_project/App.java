ebb25d2a8226573cb8f54afcf14d3283d2d45a9c
pd05484 <jan.reiners@provinzial.com>
2018-03-02 14:30:50 +0100

    Hello World 2

--- hello-world-project/src/main/java/com/test/hello_world_project/App.java
+++ hello-world-project/src/main/java/com/test/hello_world_project/App.java
@@ -3,5 +3,7 @@ package com.test.hello_world_project;
 public class App {
    public static void main(String[] args) {
       System.out.println("Hello World!");
+
+      System.out.println("Hello World 2!");
    }
 }
